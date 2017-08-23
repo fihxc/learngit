e972e59dfcb5c83ff63905898933dc5260c3cc4e
fihxc <fihxc@qq.com>
2017-08-23 20:08:41 +0800

    add feature vulcan

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const int LLINF = 0x3f3f3f3f3f3f3f3f;
+const double PI = 3.141592653589793;
+
 int main()
 {
 	cout << "edit by vim80" << endl;
